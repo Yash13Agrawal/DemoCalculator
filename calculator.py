2a37d3783d18080a503ee58aaac2b767c8e6fe36
anchal-csv <singh30anchal@gmail.com>
2025-03-24 09:49:47 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,12 @@
-
+class Calculator:
+ @staticmethod
+ def divide(a, b):
+  if b == 0:
+    return "Error: Division by zero is not allowed."
+  return a / b
+        
+             # Sample input
+if __name__ == "__main__":
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    print("Division:", Calculator.divide(num1, num2))
