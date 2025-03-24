9043632e0244f687ffe28c67f06ad674122ee4ce
Yash13Agrawal <155071356+Yash13Agrawal@users.noreply.github.com>
2025-03-24 10:16:31 +0530

    Delete addition.py

--- addition.py
+++
@@ -1,8 +0,0 @@
-def add(a, b):
-    if type(a) == int and type(b)==int:
-        return a+b
-    if type(a) == str and type(b) == str:
-        return str+str
-    
-print(add(1,2))
-print(add("Sneha", "Aneja"))  
